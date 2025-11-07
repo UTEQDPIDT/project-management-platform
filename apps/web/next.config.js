3cfad325404682f41bb7519d6fc351006ca69d80
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-07 12:43:42 -0600

    some modifications to dockerfiles

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     allowedDevOrigins: ['http://localhost:3000'],
+    // Enable standalone output for Docker
+    output: 'standalone',
 };
 
 export default nextConfig;
