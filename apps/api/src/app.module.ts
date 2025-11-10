f5cd5daa59199047f7e886f0ea3abcb8a05a51eb
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-10 14:38:14 -0600

    setup mongodb and mongoose

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Permite acceso a las variables de entorno en toda la app
     }),
+
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
