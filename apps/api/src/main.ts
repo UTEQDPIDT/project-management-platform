e9bba9d11c0ae2630a0cd4433722c9006dd0c1c8
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-11 10:11:10 -0600

    setup openapi docs with swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: ['docs', 'docs-json'],
+  });
 
+  /**
+   * CORS configuration
+   */
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
@@ -13,6 +19,26 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
+  /**
+   * API Swagger Documentation
+   */
+  const config = new DocumentBuilder()
+    .setTitle('Documentation API Plataforma de Gestión de Proyectos')
+    .setDescription(
+      'Está documentación busca detallar los endpoints disponibles en la API de la Plataforma de Gestión de Proyectos.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3001);
+
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT ?? 3001}/api`,
+  );
+  console.log(
+    `Swagger UI available at: http://localhost:${process.env.PORT ?? 3001}/docs`,
+  );
 }
 bootstrap();
