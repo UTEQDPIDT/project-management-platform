5a92e4b2a3d0f7e8cc55d2f1a80f08f5d0d55ca8
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-12 11:15:55 -0600

    generate auth module, controller and service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,8 @@ import { MongooseModule } from '@nestjs/mongoose';
     }),
 
     MongooseModule.forRoot(process.env.MONGODB_URI),
+
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
