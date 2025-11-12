280b57c0852f3bb8752bdb41d35ade1916bb9b19
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-12 11:25:15 -0600

    install passport google o auth dependencies

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import googleOauthConfig from './config/google-oauth.config';
 
 @Module({
+  imports: [ConfigModule.forFeature(googleOauthConfig)],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
