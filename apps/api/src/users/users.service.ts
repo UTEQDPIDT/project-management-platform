edbc377a623a494132fb5aed6b2cc91b7c863102
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-13 14:50:07 -0600

    add users service mongoose operations

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from '../schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const createdUser = new this.userModel(createUserDto);
+      return await createdUser.save();
+    } catch (err: any) {
+      // Handle duplicate key or validation errors
+      if (err.code === 11000) {
+        throw new BadRequestException(
+          'User with this email, matricula, or employee number already exists',
+        );
+      }
+      throw new BadRequestException(err.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<User> {
+    const user = await this.userModel.findById(id).exec();
+    if (!user) throw new NotFoundException(`User with ID: ${id} not found`);
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    try {
+      const updatedUser = await this.userModel
+        .findByIdAndUpdate(id, updateUserDto, { new: true })
+        .exec();
+
+      if (!updatedUser)
+        throw new NotFoundException(`User with ID: ${id} not found`);
+
+      return updatedUser;
+    } catch (err: any) {
+      if (err.code === 11000) {
+        throw new BadRequestException(
+          'Use: anyr with this matricula or employee number already exists',
+        );
+      }
+      throw new BadRequestException(err.message);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string): Promise<User> {
+    const deletedUser = await this.userModel.findByIdAndDelete(id).exec();
+
+    if (!deletedUser)
+      throw new NotFoundException(`User with ID: ${id} not found`);
+
+    return deletedUser;
   }
 }
