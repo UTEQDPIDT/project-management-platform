228b05f5c5369244fd8f01db653d8e422d61b2a1
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-13 14:25:56 -0600

    exclude google's provided attributes from oauth scope for updating

--- apps/api/src/users/dto/update-user.dto.ts
+++ apps/api/src/users/dto/update-user.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  // Exclude read-only fields
+  @IsOptional()
+  readonly email?: never;
+
+  @IsOptional()
+  readonly avatarUrl?: never;
+
+  @IsOptional()
+  readonly givenName?: never;
+
+  @IsOptional()
+  readonly familyName?: never;
+}
