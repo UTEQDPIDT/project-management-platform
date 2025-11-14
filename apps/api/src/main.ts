54acbecbed533dc7fd49b90b7ac7978c79765040
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-14 08:57:18 -0600

    update app base route log

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3001);
 
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Application is running on: ${await app.getUrl()}/api`);
   console.log(`Swagger UI available at: ${await app.getUrl()}/docs`);
   console.log(`Swagger JSON available at: ${await app.getUrl()}/docs-json`);
 }
