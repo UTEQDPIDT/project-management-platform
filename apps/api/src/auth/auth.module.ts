3dd460a8e271b58ef3aa536011275a794e5c1c92
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-14 11:42:12 -0600

    add jwt config file to module

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -4,9 +4,14 @@ import { AuthController } from './auth.controller';
 import { ConfigModule } from '@nestjs/config';
 import googleOauthConfig from './config/google-oauth.config';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './config/jwt.config';
 
 @Module({
-  imports: [ConfigModule.forFeature(googleOauthConfig)],
+  imports: [
+    ConfigModule.forFeature(googleOauthConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   providers: [AuthService, GoogleStrategy],
   controllers: [AuthController],
 })
