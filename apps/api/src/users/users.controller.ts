2675105865d1ef79e1b0c3f9ddab746ebd79c650
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-14 09:07:10 -0600

    add openapi decorators to controller

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -10,31 +10,46 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiCreatedResponse({ description: 'Usuario creado correctamente.' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiOkResponse({ description: 'Lista de usuarios obtenida correctamente.' })
+  @ApiNotFoundResponse({ description: 'No se encontraron usuarios.' })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @ApiOkResponse({ description: 'Usario obtenido correctamente.' })
+  @ApiNotFoundResponse({ description: 'No se encontro al usuario.' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+  @ApiOkResponse({ description: 'Usuario actualizado correctamente.' })
+  @ApiNotFoundResponse({ description: 'No se encontro al usuario.' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @ApiNotFoundResponse({ description: 'No se encontro al usuario.' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
