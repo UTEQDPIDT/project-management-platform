d0a341ee8380ffd8a6d377f02920c9a93d903ece
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-24 12:36:19 -0600

    add role based path nextjs middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,12 +1,43 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { jwtVerify } from 'jose';
+import { UserRole } from '@/types/user-roles.enum';
 
-export function middleware(req: NextRequest) {
-  //   const token = req.cookies.get('token')?.value;
+const SECRET = new TextEncoder().encode(process.env.NEXT_PRIVATE_JWT_SECRET!);
 
-  const url = req.nextUrl.pathname;
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get('accessToken')?.value;
+  const { pathname } = req.nextUrl;
 
-  return NextResponse.next();
+  // If no token redirect to home page
+  if (!token) {
+    return NextResponse.redirect(new URL('/?expired=true', req.url));
+  }
+
+  // Verify token
+  try {
+    const { payload } = await jwtVerify(token, SECRET);
+    const role = payload.role;
+
+    // Protect admin routes
+    if (pathname.startsWith('/admin')) {
+      if (role !== UserRole.ADMIN) {
+        return NextResponse.redirect(new URL('/', req.url));
+      }
+    }
+
+    // Protect user routes
+    if (pathname.startsWith('/user')) {
+      if (role !== UserRole.USER) {
+        return NextResponse.redirect(new URL('/', req.url));
+      }
+    }
+
+    // If all checks pass, continue to the requested page
+    return NextResponse.next();
+  } catch (err) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
 }
 
 export const config = {
