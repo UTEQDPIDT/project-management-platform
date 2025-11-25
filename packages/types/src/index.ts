5bba6d27eb7e22030155c2be9469022078f1b5ab
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-25 14:32:01 -0600

    move teams-grade enum to centralized package and update imports

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,6 @@
 export * from './enums/user-role.enum';
 export * from './enums/user-type.enum';
 export * from './enums/career-level.enum';
+export * from './enums/teams-grade.enum';
 
 export * from './user.type';
