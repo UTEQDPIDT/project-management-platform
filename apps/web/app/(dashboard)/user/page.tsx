466bc1263cf0f3574e4ee1e23ff00a6b96f33faf
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-25 09:58:58 -0600

    add header to component library

--- apps/web/app/(dashboard)/user/page.tsx
+++ apps/web/app/(dashboard)/user/page.tsx
@@ -1,10 +1,33 @@
-import { Header } from '@/components/header';
+import {
+  Header,
+  HeaderAction,
+  HeaderContent,
+  HeaderDescription,
+  HeaderHeading,
+  HeaderTitle,
+} from '@/components/header';
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Page = () => {
   return (
     <div>
-      <Header />
+      <Header>
+        <HeaderHeading>
+          <HeaderTitle>Dashboard</HeaderTitle>
+          <HeaderDescription>
+            Bienvenido de vuelta Aeon Julien
+          </HeaderDescription>
+        </HeaderHeading>
+        <HeaderContent>
+          <Button>Button</Button>
+          <Button>Button</Button>
+        </HeaderContent>
+        <HeaderAction>
+          <Button>Button</Button>
+          <Button>Button</Button>
+        </HeaderAction>
+      </Header>
     </div>
   );
 };
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,18 +1,98 @@
-import React from 'react';
+import * as React from 'react';
 import { SidebarTrigger } from './ui/sidebar';
 import { Separator } from './ui/separator';
 
-export function Header() {
+import { cn } from '@/lib/utils';
+
+function Header({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<'header'>) {
   return (
-    <header className="flex shrink-0 items-center border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 py-2 lg:gap-2">
+    <header
+      data-slot="header"
+      className="flex shrink-0 items-center border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
+      {...props}
+    >
+      <div
+        className={cn(
+          'flex w-full items-center gap-1 px-4 py-2 lg:gap-2',
+          className,
+        )}
+      >
         <SidebarTrigger />
         <Separator
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-lg font-medium">Dashboard</h1>
+        {children}
       </div>
     </header>
   );
 }
+
+function HeaderHeading({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot="header-title"
+      className={cn('flex flex-col', className)}
+      {...props}
+    />
+  );
+}
+
+function HeaderTitle({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot="header-title"
+      className={cn('leading-none text-lg font-semibold', className)}
+      {...props}
+    />
+  );
+}
+
+function HeaderDescription({
+  className,
+  ...props
+}: React.ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot="header-description"
+      className={cn('text-sm text-muted-foreground', className)}
+      {...props}
+    />
+  );
+}
+
+function HeaderContent({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot="header-content"
+      className={cn('flex gap-2 px-2 items-center justify-start', className)}
+      {...props}
+    />
+  );
+}
+
+function HeaderAction({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot="header-action"
+      className={cn(
+        'ml-auto flex gap-2 items-center justify-center',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export {
+  Header,
+  HeaderHeading,
+  HeaderTitle,
+  HeaderDescription,
+  HeaderContent,
+  HeaderAction,
+};
