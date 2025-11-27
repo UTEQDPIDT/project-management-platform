521a29cc52e06d81906c3d3cbfe2f5869d88e0ac
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-27 14:46:01 -0600

    document controller and dto

--- apps/api/src/products/dto/create-product.dto.ts
+++ apps/api/src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CoAuthor } from '@repo/types';
 import {
   IsString,
@@ -8,24 +9,38 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({ description: 'Nombre del producto' })
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'Categoría a la que pertenece el producto' })
   @IsString()
   category: string;
 
+  @ApiProperty({ description: 'Subcategoría a la que pertenece el producto' })
   @IsString()
   subcategory: string;
 
+  @ApiProperty({
+    description: 'Detalles del producto como especifica la documentación',
+    maxLength: 255,
+    required: false,
+  })
   @IsOptional()
   @IsString()
   @MaxLength(255)
   details?: string;
 
+  @ApiProperty({
+    description: 'Nivel del Co-Autor',
+  })
   @IsEnum(CoAuthor)
   @IsOptional()
   coAuthor?: CoAuthor;
 
+  @ApiProperty({
+    description: 'Evidencias asociadas al producto, referenciadas por IDs',
+  })
   @IsOptional()
   @IsMongoId({ each: true })
   files?: string[];
--- apps/api/src/products/products.controller.ts
+++ apps/api/src/products/products.controller.ts
@@ -10,31 +10,58 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import {
+  ApiAcceptedResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiCreatedResponse({
+    description: 'Producto creado correctamente.',
+    type: CreateProductDto,
+  })
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
+  @ApiAcceptedResponse({
+    description: 'Lista de productos obtenida correctamente.',
+    type: [CreateProductDto],
+  })
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
 
+  @ApiAcceptedResponse({
+    description: 'Producto obtenido correctamente.',
+    type: CreateProductDto,
+  })
+  @ApiNotFoundResponse({ description: 'No se encontro el producto.' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
+  @ApiAcceptedResponse({
+    description: 'Productp actualizado correctamente.',
+    type: UpdateProductDto,
+  })
+  @ApiNotFoundResponse({ description: 'No se encontro el producto.' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
+  @ApiAcceptedResponse({ description: 'Producto eliminado correctamente.' })
+  @ApiNotFoundResponse({ description: 'No se encontro el producto.' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
