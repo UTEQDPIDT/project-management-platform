e98110fa0a6f764fce3a8239b49c8d579a101d87
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-27 11:14:21 -0600

    remove unused import

--- apps/api/src/files/files.service.ts
+++ apps/api/src/files/files.service.ts
@@ -5,7 +5,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { File } from '../schemas/file.schema';
 import { mongo } from 'mongoose';
-const GridFSBucket = mongo.GridFSBucket;
 
 @Injectable()
 export class FilesService {
