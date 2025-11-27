dcee941f783fdc19a36d9c4dda49a8da7bf66a6f
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-27 14:15:06 -0600

    create products dto

--- apps/api/src/products/dto/create-product.dto.ts
+++ apps/api/src/products/dto/create-product.dto.ts
@@ -1,1 +1,32 @@
-export class CreateProductDto {}
+import { CoAuthor } from '@repo/types';
+import {
+  IsString,
+  IsOptional,
+  IsMongoId,
+  IsEnum,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  category: string;
+
+  @IsString()
+  subcategory: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  details?: string;
+
+  @IsEnum(CoAuthor)
+  @IsOptional()
+  coAuthor?: CoAuthor;
+
+  @IsOptional()
+  @IsMongoId({ each: true })
+  files?: string[];
+}
