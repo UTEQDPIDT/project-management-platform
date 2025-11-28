dc8d7dd327f79b2188f6fe4b80d2e619c7740299
Aeon Clouzeau <aeonruizc@gmail.com>
2025-11-28 12:25:49 -0600

    define project schema, add impact level enum

---
+++ apps/api/src/schemas/project.schema.ts
@@ -0,0 +1,154 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Status, ImpactLevel } from '@repo/types';
+import mongoose, { Document } from 'mongoose';
+import { User } from './user.schema';
+import { Team } from './team.schema';
+import { Activity } from './activities.schema';
+import { Product } from './product.schema';
+import { File } from './file.schema';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Schema({ timestamps: true })
+export class Project extends Document {
+  @ApiProperty({
+    description: 'Nombre del proyecto.',
+  })
+  @Prop({ required: true })
+  name: string;
+
+  @ApiProperty({
+    description: 'Resumen del proyecto.',
+  })
+  @Prop({ required: true, maxLength: 500 })
+  summary: string;
+
+  @ApiProperty({
+    description: 'Objetivo principal del proyecto.',
+  })
+  @Prop({ required: true, maxLength: 500 })
+  objective: string;
+
+  @ApiProperty({
+    description: 'Nivel de maduración del Proyecto.',
+  })
+  @Prop({ required: true, min: 1, max: 9 })
+  trlRating: number;
+
+  @ApiProperty({
+    description: 'Estado actual del proyecto.',
+  })
+  @Prop({
+    required: true,
+    type: String,
+    enum: Object.values(Status),
+    default: Status.PENDING,
+  })
+  status: Status;
+
+  @ApiProperty({
+    description:
+      'Progreso general del proyecto, promedio de actividades completadas.',
+  })
+  @Prop({ required: true, min: 0, max: 100 })
+  progress: number;
+
+  @ApiProperty({
+    description: 'Categorías a las que pertenece el proyecto.',
+  })
+  @Prop({})
+  category: string;
+
+  @ApiProperty({
+    description: 'Áreas de conocimiento que alude el proyecto.',
+  })
+  @Prop({})
+  knowledgeAreas: string;
+
+  @ApiProperty({
+    description: 'Áreas de impacto del proyecto.',
+  })
+  @Prop({})
+  impactAreas: string;
+
+  @ApiProperty({
+    description: 'Prioridades Nacionales.',
+  })
+  @Prop({})
+  prioritiesPND: string;
+
+  @ApiProperty({
+    description: 'Objetivos sustentables a los que apunta el proyecto.',
+  })
+  @Prop({})
+  sustainableObjectives: string;
+
+  @ApiProperty({
+    description: 'Lineas de innovación a las que se alinea el proyecto.',
+  })
+  @Prop({})
+  innovationLines: string;
+
+  @ApiProperty({
+    description: 'Organización a la que le pertenece el proyecto.',
+  })
+  @Prop({})
+  organization: string;
+
+  @ApiProperty({
+    description: 'Niveles de impacto del proyecto.',
+  })
+  @Prop({
+    required: true,
+    type: String,
+    enum: Object.values(ImpactLevel),
+    default: ImpactLevel.LOCAL,
+  })
+  impactLevel: string;
+
+  @ApiProperty({
+    description: 'Dueño del proyecto.',
+  })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  owner: User;
+
+  @ApiProperty({
+    description: 'Equipo que trabaja en el proyecto.',
+  })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Team' })
+  team: Team;
+
+  @ApiProperty({
+    description: 'Otros proyectos relacionados o previos al proyecto.',
+  })
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }] })
+  relatedProjects: Project[];
+
+  @ApiProperty({
+    description: 'Actividades relacionadas al proyecto.',
+  })
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Activity' }] })
+  activities: Activity[];
+
+  @ApiProperty({
+    description: 'Productos relacionados al proyecto.',
+  })
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }] })
+  products: Product[];
+
+  @ApiProperty({
+    description: 'Archivos relacionados al proyecto',
+  })
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'File' }] })
+  files: File[];
+
+  @ApiProperty({
+    description:
+      'Fecha de vencimiento de la actividad. Al existir una fecha final de vencimiento, esta será el inicio del plazo.',
+  })
+  @Prop()
+  dueDate?: Date;
+
+  @ApiProperty({ description: 'Fecha final de vencimiento de la actividad' })
+  @Prop()
+  dueDateEnd?: Date;
+}
