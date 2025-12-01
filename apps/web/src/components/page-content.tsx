d7c3345c573753212ba75d8dbaf2980abc7d82c4
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-01 09:49:00 -0600

    add page content container

---
+++ apps/web/src/components/page-content.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+
+export function PageContent({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={cn('flex flex-col px-4 py-2.5', className)}>{children}</div>
+  );
+}
