d83882262fe71d4b1ecd4df64bb05073caa867ac
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-01 10:15:57 -0600

    update user dto

--- apps/api/src/users/dto/update-user.dto.ts
+++ apps/api/src/users/dto/update-user.dto.ts
@@ -1,16 +1,30 @@
 import {
   IsEnum,
+  IsInt,
   IsNumberString,
   IsOptional,
   IsString,
   Length,
+  Max,
+  Min,
 } from 'class-validator';
-import { UserRole, CareerLevel } from '@repo/types';
+import { UserRole, CareerLevel, Gender, State } from '@repo/types';
 
 export class UpdateUserDto {
   @IsOptional()
   @IsEnum(UserRole)
   role: UserRole;
+
+  @IsEnum(Gender)
+  gender: Gender;
+
+  @IsEnum(State)
+  state: State;
+
+  @Min(17)
+  @Max(100)
+  @IsInt()
+  age: number;
 
   @IsOptional()
   @IsNumberString()
