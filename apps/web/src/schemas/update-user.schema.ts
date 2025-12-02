9d501d3af4a6d34ce2fd40f24b331da3b9ea75a7
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-02 11:37:49 -0600

    define zod schema to update user profile

---
+++ apps/web/src/schemas/update-user.schema.ts
@@ -0,0 +1,28 @@
+import { CareerLevel, Gender, State, UserType } from '@repo/types';
+import { z } from 'zod';
+import { mongoId } from '@/lib/utils';
+
+export const updateUserSchema = z.object({
+  type: z.enum(UserType).optional(),
+  gender: z.enum(Gender),
+  state: z.enum(State),
+  dateOfBirth: z.date(),
+  matricula: z
+    .string()
+    .regex(/^[0-9]+$/, 'La matrícula debe contener solo números')
+    .length(10, 'La matricula debe tener 10 caracteres')
+    .trim()
+    .optional(),
+  employeeNumber: z
+    .string()
+    .regex(/^[0-9]+$/, 'El número de empleado debe contener solo números')
+    .min(5, 'El número de empleado debe tener un mínimo de 5 caracteres')
+    .max(10, 'El número de empleado debe tener un máximo de 10 caracteres')
+    .trim()
+    .optional(),
+  careerLevel: z.enum(CareerLevel).optional(),
+  division: mongoId.optional(),
+  educationalProgram: mongoId.optional(),
+});
+
+export type UpdateUser = z.infer<typeof updateUserSchema>;
