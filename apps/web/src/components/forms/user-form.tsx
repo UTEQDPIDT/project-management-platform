bf81a84e4c4092cb3285a9d28e2fc38410def37a
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-02 11:46:56 -0600

    set react hook form base

--- apps/web/src/components/forms/user-form.tsx
+++ apps/web/src/components/forms/user-form.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+
+import { UpdateUser, updateUserSchema } from '@/schemas/update-user.schema';
+
 import React from 'react';
 
 export default function UserForm() {
-  return <div></div>;
+  const form = useForm<UpdateUser>({
+    resolver: zodResolver(updateUserSchema),
+  });
+
+  const onSubmit = (data: UpdateUser) => {
+    // handle user update
+  };
+
+  return <form onSubmit={form.handleSubmit(onSubmit)}></form>;
 }
