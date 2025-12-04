9a83a8a97c42b59acabc0cf6307564efeab754d0
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-04 16:03:45 -0600

    refac: user schema to make members and collaborators store emails

--- apps/web/src/schemas/team.schema.ts
+++ apps/web/src/schemas/team.schema.ts
@@ -3,11 +3,11 @@ import { TeamsGrade } from '@repo/types';
 import { z } from 'zod';
 
 export const teamSchema = z.object({
-  teamName: z.string().max(50, 'Excede el máximo de 255 carecteres'),
+  teamName: z.string().max(50, 'Excede el máximo de 50 carecteres'),
   division: mongoId.or(z.literal('')),
   summary: z.string().max(255, 'Excede el máximo de 255 carecteres').optional(),
   grade: z.enum(TeamsGrade),
-  collaborators: mongoId.array(),
-  members: mongoId.array(),
+  collaborators: z.array(z.string().email('Correo inválido')).optional(),
+  members: z.array(z.string().email('Correo inválido')).optional(),
   isPrivate: z.boolean(),
 });
