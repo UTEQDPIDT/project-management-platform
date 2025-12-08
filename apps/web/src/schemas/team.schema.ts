80f5826e9ee2f6f430fdbfab19e81bdd9d246aec
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-08 10:50:40 -0600

    add institutional email validation

--- apps/web/src/schemas/team.schema.ts
+++ apps/web/src/schemas/team.schema.ts
@@ -2,12 +2,24 @@ import { mongoId } from '@/lib/utils';
 import { TeamsGrade } from '@repo/types';
 import { z } from 'zod';
 
+const uteqEmail = /^[A-Za-z0-9._%+-]+@uteq\.edu\.mx$/i;
+
 export const teamSchema = z.object({
   teamName: z.string().max(50, 'Excede el máximo de 50 carecteres'),
   division: mongoId.or(z.literal('')),
   summary: z.string().max(255, 'Excede el máximo de 255 carecteres').optional(),
   grade: z.enum(TeamsGrade),
-  collaborators: z.array(z.string().email('Correo inválido')),
-  members: z.array(z.string().email('Correo inválido')),
+  collaborators: z.array(
+    z
+      .string()
+      .email('Correo inválido')
+      .regex(uteqEmail, 'El correo debe ser institucional'),
+  ),
+  members: z.array(
+    z
+      .string()
+      .email('Correo inválido')
+      .regex(uteqEmail, 'El correo debe ser institucional'),
+  ),
   isPrivate: z.boolean(),
 });
