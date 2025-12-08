35e470240e8f56b7b93ca930ca9bd6126ecba837
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-08 11:51:57 -0600

    add redirection to teams dashboard

--- apps/web/app/(dashboard)/user/equipos/crear/page.tsx
+++ apps/web/app/(dashboard)/user/equipos/crear/page.tsx
@@ -13,22 +13,24 @@ import { Button } from '@/components/ui/button';
 import React from 'react';
 import { Plus } from 'lucide-react';
 import { useAllTeams } from '@/hooks/team';
-import LoadingMessage from '@/components/loading-message';
 import { CreateTeamForm } from '@/components/forms/create-team-form';
+import Link from 'next/link';
 
 const Page = () => {
-  const { data: teams, isLoading: loadingTeams } = useAllTeams();
-  console.log('TEAMS', teams);
-
   return (
     <div>
       <Header>
         <HeaderHeading>
           <HeaderTitle>Crear Equipo</HeaderTitle>
           <HeaderDescription>
-            Encuentra equipos y gestiona los equipos a los que perteneces.
+            Llena los detalles del nuevo equipo e invita a los integrantes.
           </HeaderDescription>
         </HeaderHeading>
+        <HeaderAction>
+          <Button asChild variant="outline">
+            <Link href={'/user/equipos'}>Cancelar</Link>
+          </Button>
+        </HeaderAction>
       </Header>
       <PageContent className="items-center">
         <CreateTeamForm />
