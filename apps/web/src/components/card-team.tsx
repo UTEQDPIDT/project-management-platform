c46efd51d9234233dc129a0c937d6a8ed577dd80
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-09 10:24:10 -0600

    add badge to team card

--- apps/web/src/components/card-team.tsx
+++ apps/web/src/components/card-team.tsx
@@ -9,11 +9,11 @@ import {
   CardAction,
 } from './ui/card';
 import { Button } from './ui/button';
-import { ITeam } from '@repo/types';
+import { ITeam, TeamsGrade } from '@repo/types';
 import { User, UserPlus, Users } from 'lucide-react';
 import IconSquare from './icon-square';
-import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar';
 import AvatarRow from './avatar-row';
+import { Badge } from './ui/badge';
 
 export default function CardTeam({
   teamName,
@@ -26,17 +26,30 @@ export default function CardTeam({
   ITeam,
   'teamName' | 'summary' | 'division' | 'grade' | 'collaborators' | 'members'
 >) {
+  let badgeStyle;
+  switch (grade) {
+    case TeamsGrade.FORMACION:
+      badgeStyle = 'bg-gray-200 text-gray-800 ';
+      break;
+    case TeamsGrade.CONSOLIDADO:
+      badgeStyle = 'bg-green-100 text-green-800';
+      break;
+  }
+
   return (
     <Card className="max-w-md">
       <CardHeader>
-        <div className="flex gap-2 items-center">
-          <IconSquare>
-            <Users />
-          </IconSquare>
-          <div className="flex flex-col gap-1">
-            <CardTitle>{teamName}</CardTitle>
-            <CardDescription className="text-xs">{division}</CardDescription>
+        <div className="flex justify-between">
+          <div className="flex gap-2 items-center">
+            <IconSquare>
+              <Users />
+            </IconSquare>
+            <div className="flex flex-col gap-1">
+              <CardTitle>{teamName}</CardTitle>
+              <CardDescription className="text-xs">{division}</CardDescription>
+            </div>
           </div>
+          <Badge className={`${badgeStyle} h-6`}>{grade}</Badge>
         </div>
       </CardHeader>
       <CardContent>
