df9ee8377d67f1cd98bbf681e3f0d0791c5858d8
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-17 19:15:06 -0600

    add utils function to calculate trl rating, create trl assesment zod schema

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,12 +1,43 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-
 import { z } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * TRL Rating
+ */
+export const TRL_QUESTIONS = {
+  1: ['Basic principles observed'],
+  2: ['Technology concept formulated'],
+  3: ['Experimental proof of concept'],
+  4: ['Technology validated in lab'],
+  5: ['Technology validated in relevant environment'],
+  6: ['Technology demonstrated in relevant environment'],
+  7: ['System prototype demonstrated in operational environment'],
+  8: ['System complete and qualified'],
+  9: ['System proven in operational environment'],
+} as const;
+
+export function calculateTRL(answers: Record<string, boolean>): number {
+  let trl = 0;
+
+  for (let level = 1; level <= 9; level++) {
+    const questions = TRL_QUESTIONS[level as keyof typeof TRL_QUESTIONS];
+
+    const passed = questions.every((q) => answers[q] === true);
+    if (passed) trl = level;
+    else break;
+  }
+
+  return trl;
+}
+
+/**
+ * Zod Schema Validations
+ */
 // MongoID regex
 export const mongoId = z
   .string()
