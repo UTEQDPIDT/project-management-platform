b7fa7165d031f4d1cbf2919dc5c6a567334edf07
Aeon Clouzeau <aeonruizc@gmail.com>
2025-12-19 16:25:13 -0600

    add product services to frontend

---
+++ apps/web/src/services/product.service.ts
@@ -0,0 +1,65 @@
+import { api } from '@/lib/axios';
+import { IProduct } from '@repo/types';
+
+const createProduct = async (
+  productData: Pick<
+    IProduct,
+    'name' | 'category' | 'subcategory' | 'details' | 'coAuthor'
+  >,
+) => {
+  try {
+    const { data } = await api.post('/products', productData);
+    return data;
+  } catch (err) {
+    console.error('Error creating product', err);
+  }
+};
+
+const getProducts = async () => {
+  try {
+    const { data } = await api.get('/products');
+    return data;
+  } catch (err) {
+    console.error('Error fetching all products', err);
+  }
+};
+
+const getProductById = async (id: string) => {
+  try {
+    const { data } = await api.get(`/products/${id}`);
+    return data;
+  } catch (err) {
+    console.error(`Error fetching product by ID: ${id}`, err);
+  }
+};
+
+const updateProduct = async (
+  id: string,
+  productData: Pick<
+    IProduct,
+    'name' | 'category' | 'subcategory' | 'details' | 'coAuthor'
+  >,
+) => {
+  try {
+    const { data } = await api.patch(`/products/${id}`, productData);
+    return data;
+  } catch (err) {
+    console.error(`Error updating product with ID: ${id}`, err);
+  }
+};
+
+const deleteProduct = async (id: string) => {
+  try {
+    await api.delete(`/products/${id}`);
+  } catch (err) {
+    console.error(`Error deleting product ID ${id}`, err);
+  }
+};
+
+export {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+};
