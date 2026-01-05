390525d81a42161b12c206450d5e0f78b2fbd3c1
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-04 18:38:20 -0600

    add Empty component when there are no projects

--- apps/web/app/(dashboard)/user/proyectos/page.tsx
+++ apps/web/app/(dashboard)/user/proyectos/page.tsx
@@ -10,12 +10,20 @@ import {
 } from '@/components/header';
 import { PageContent } from '@/components/page-content';
 import { Button } from '@/components/ui/button';
-import { Plus } from 'lucide-react';
+import { Folder, Plus } from 'lucide-react';
 import Link from 'next/link';
 import { useProjectsByOwner } from '@/hooks/projects/use-projects-by-owner';
 import LoadingMessage from '@/components/loading-message';
 import { IProject } from '@repo/types';
 import { ProjectCard } from '@/components/project-card';
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty';
 
 const Page = () => {
   const { data: projects, isLoading: loadingProjects } = useProjectsByOwner();
@@ -29,23 +37,40 @@ const Page = () => {
             Administra y dale seguimiento a tus proyectos.
           </HeaderDescription>
         </HeaderHeading>
+
         <HeaderAction>
           <Button asChild>
-            <Link href={'/user/proyectos/crear'}>
-              <Plus /> Crear Proyecto
-            </Link>
+            <Link href={'/user/proyectos/crear'}>Crear Proyecto</Link>
           </Button>
         </HeaderAction>
       </Header>
       <PageContent>
         {loadingProjects ? (
           <LoadingMessage message="Cargando Proyectos" />
-        ) : (
+        ) : projects.length > 0 ? (
           <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4">
             {projects.map((p: IProject) => (
               <ProjectCard key={p._id} project={p} />
             ))}
           </div>
+        ) : (
+          <Empty>
+            <EmptyHeader>
+              <EmptyMedia variant="icon">
+                <Folder />
+              </EmptyMedia>
+              <EmptyTitle>No Tienes Proyectos</EmptyTitle>
+              <EmptyDescription>
+                No haz creado ningun proyecto. Inicia creanto tu primer
+                proyecto.
+              </EmptyDescription>
+              <EmptyContent>
+                <Button asChild>
+                  <Link href={'/user/proyectos/crear'}>Crear Proyecto</Link>
+                </Button>
+              </EmptyContent>
+            </EmptyHeader>
+          </Empty>
         )}
       </PageContent>
     </div>
