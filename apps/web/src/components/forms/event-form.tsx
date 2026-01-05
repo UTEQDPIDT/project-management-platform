82b8ccef627f99738ea6dfc8fe8acecf53c2a57c
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-04 22:06:12 -0600

    add participants field, conditional rendering of submit buttons to event form

--- apps/web/src/components/forms/event-form.tsx
+++ apps/web/src/components/forms/event-form.tsx
@@ -40,43 +40,91 @@ import {
 } from '../ui/select';
 import { Separator } from '../ui/separator';
 import { eventSchema } from '@/schemas/event.schema';
+import { EventType, IEvent, IUser } from '@repo/types';
+import { Switch } from '../ui/switch';
+import { DatePicker } from '../ui/date-picker';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import LoadingMessage from '../loading-message';
+import { Check, ChevronsUpDown } from 'lucide-react';
+import { Avatar } from '../ui/avatar';
+import { ProfileInfo } from '../profile-info';
+import { useGetAllUsers } from '@/hooks/user';
 
 interface EventFormProps {
-  event?: string;
+  event?: IEvent;
 }
 
 export default function EventForm({ event }: EventFormProps) {
   const router = useRouter();
 
   /**
-   * Tanstack Hooks
+   * TANSTACK HOOKS
    */
+  const { data: users, isLoading: loadingUsers } = useGetAllUsers();
+
+  const renderButton = (event: IEvent | undefined) => {
+    if (event) {
+      return (
+        <Button type="submit" disabled={false}>
+          {false ? (
+            <LoadingMessage message="Actualizando evento" />
+          ) : (
+            'Actualizar evento'
+          )}
+        </Button>
+      );
+    } else {
+      return (
+        <Button type="submit" disabled={false}>
+          {false ? <LoadingMessage message="Creando evento" /> : 'Crear evento'}
+        </Button>
+      );
+    }
+  };
 
   const form = useForm({
     resolver: zodResolver(eventSchema),
     mode: 'onChange',
     defaultValues: {
-      name: '',
-      summary: '',
-      location: '',
-      organization: '',
-      date: undefined,
+      name: event?.name || '',
+      summary: event?.summary || '',
+      type: event?.type || EventType.INTERNO,
+      location:
+        event?.location || EventType.INTERNO
+          ? 'Av. Pie de la Cuesta 2501, Nacional, 76148 Santiago de Querétaro, Qro.'
+          : '',
+      organization: event?.organization || EventType.INTERNO ? 'UTEQ' : '',
+      startDate: event?.startDate || undefined,
+      endDate: event?.endDate || undefined,
+      isPrivate: event?.isPrivate || false,
+      participants: event?.participants
+        ? event.participants.map((p: IUser) => p._id)
+        : [],
     },
   });
 
   /**
-   * Handlers
+   * HANDLERS
    */
-
   const onSubmit = async (data: z.infer<typeof eventSchema>) => {
     try {
       const cleanedData = {
         ...data,
+        organization: data.organization === '' ? undefined : data.organization,
       };
+      console.log('CLEANED DATA', cleanedData);
 
-      // mutations
-      // form.reset();
-      // router.push('/user/proyectos');
+      if (event) {
+        // update
+      } else {
+        // create
+      }
+
+      if (true) {
+        // form.reset();
+        // router.push('/admin/eventos');
+      }
     } catch (err) {
       console.error('Error cleaning data', err);
     }
@@ -87,11 +135,328 @@ export default function EventForm({ event }: EventFormProps) {
   };
 
   return (
-    <div>
-      <form
-        className="flex flex-col gap-6 md:w-2xl"
-        onSubmit={form.handleSubmit(onSubmit, onError)}
-      ></form>
-    </div>
+    <form
+      className="flex flex-col gap-6 md:w-2xl"
+      onSubmit={form.handleSubmit(onSubmit, onError)}
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle>Datos generales del evento</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <FieldGroup>
+            <Controller
+              control={form.control}
+              name="name"
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Nombre *</FieldLabel>
+                  <InputGroup>
+                    <InputGroupInput
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Ingresa el nombre del evento"
+                    />
+                  </InputGroup>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              control={form.control}
+              name="summary"
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Descripción</FieldLabel>
+                  <InputGroup>
+                    <InputGroupTextarea
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="e.g. La Universidad Tecnológica de Querétaro (UTEQ) y el Centro de Ingeniería y Desarrollo Industrial (CIDESI) se enorgullecen en presentar el 4to. Congreso Internacional de Manufactura Inteligente, Mecatrónica y Elementos Tecnológicos de la Industria 4.0. donde..."
+                    />
+                    <InputGroupAddon align="block-end">
+                      {field.value?.length}/500
+                    </InputGroupAddon>
+                  </InputGroup>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              control={form.control}
+              name="type"
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Tipo de evento</FieldLabel>
+                  <Select {...field} onValueChange={field.onChange}>
+                    <SelectTrigger
+                      id={field.name}
+                      onBlur={field.onBlur}
+                      aria-invalid={fieldState.invalid}
+                    >
+                      <SelectValue placeholder="Estado" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {Object.values(EventType).map((type) => (
+                        <SelectItem key={type} value={type}>
+                          {type}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              control={form.control}
+              name="location"
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Ubicación *</FieldLabel>
+                  <InputGroup>
+                    <InputGroupInput
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Ingresa la ubicación del evento"
+                    />
+                  </InputGroup>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              control={form.control}
+              name="organization"
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>
+                    Organización encargada del evento
+                  </FieldLabel>
+                  <InputGroup>
+                    <InputGroupInput
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Ingresa el nombre de la organización"
+                    />
+                  </InputGroup>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Fechas</CardTitle>
+          <CardDescription>
+            Define las fechas del evento (la fecha de término no es
+            obligatoria).
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <FieldGroup>
+            <div className="flex gap-6 flex-col md:flex-row">
+              <Controller
+                control={form.control}
+                name="startDate"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor={field.name}>
+                      Fecha de inicio*
+                    </FieldLabel>
+                    <DatePicker date={field.value} onChange={field.onChange} />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                control={form.control}
+                name="endDate"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor={field.name}>
+                      Fecha de término
+                    </FieldLabel>
+                    <DatePicker date={field.value} onChange={field.onChange} />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </div>
+          </FieldGroup>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Asistentes</CardTitle>
+          <CardDescription>Invita a participar en el evento.</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Controller
+            control={form.control}
+            name="participants"
+            render={({ field }) => {
+              const value = field.value ?? [];
+
+              return (
+                <FieldGroup>
+                  {/* <FieldContent>
+                    <FieldLabel>Asistentes</FieldLabel>
+                    <FieldDescription>
+                      Invita a participar del proyecto.
+                    </FieldDescription>
+                  </FieldContent> */}
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className="w-full justify-between font-normal"
+                      >
+                        {value.length ? (
+                          `${value.length} seleccionados`
+                        ) : (
+                          <span className="text-muted-foreground font-normal">
+                            Sin selección
+                          </span>
+                        )}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 opacity-50" />
+                      </Button>
+                    </PopoverTrigger>
+
+                    <PopoverContent className="w-full md:w-xl max-h-96 overflow-y-auto p-0">
+                      <Command>
+                        <CommandGroup>
+                          {loadingUsers ? (
+                            <CommandItem disabled>
+                              <LoadingMessage />
+                            </CommandItem>
+                          ) : users.length > 0 ? (
+                            users.map((user: IUser) => {
+                              const selected = value.includes(user._id);
+
+                              return (
+                                <CommandItem
+                                  key={user._id}
+                                  onSelect={() => {
+                                    field.onChange(
+                                      selected
+                                        ? value.filter((v) => v !== user._id)
+                                        : [...value, user._id],
+                                    );
+                                  }}
+                                >
+                                  <Check
+                                    className={`mr-2 h-4 w-4 ${
+                                      selected ? 'opacity-100' : 'opacity-0'
+                                    }`}
+                                  />
+
+                                  <ProfileInfo
+                                    givenName={user.givenName}
+                                    familyName={user.familyName}
+                                    avatarUrl={user.avatarUrl}
+                                    size="sm"
+                                  />
+                                </CommandItem>
+                              );
+                            })
+                          ) : (
+                            <div className="w-full select-none p-2 flex items-center justify-center">
+                              <span className="text-muted-foreground text-sm">
+                                No se encontraron usuarios.
+                              </span>
+                            </div>
+                          )}
+                        </CommandGroup>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                </FieldGroup>
+              );
+            }}
+          />
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Ajustes</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <FieldGroup>
+            <Controller
+              control={form.control}
+              name="isPrivate"
+              render={({
+                field: { onChange, onBlur, ...field },
+                fieldState,
+              }) => (
+                <Field
+                  orientation={'horizontal'}
+                  data-invalid={fieldState.invalid}
+                >
+                  <FieldContent>
+                    <FieldLabel htmlFor={field.name}>Evento Privado</FieldLabel>
+                    <FieldDescription>
+                      Al hacer el evento privado, los asistentes solo podrán
+                      participar siendo invitados.
+                    </FieldDescription>
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </FieldContent>
+                  <Switch
+                    id={field.name}
+                    name={field.name}
+                    checked={field.value}
+                    onCheckedChange={onChange}
+                    aria-invalid={fieldState.invalid}
+                  />
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </CardContent>
+      </Card>
+
+      <div className="flex gap-2">
+        <Button variant={'outline'} type="button" asChild>
+          <Link href="/admin/eventos">Cancelar</Link>
+        </Button>
+        {renderButton(event)}
+      </div>
+    </form>
   );
 }
