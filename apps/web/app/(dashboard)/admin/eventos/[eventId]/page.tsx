e4c8fe3f625e381e06b054cba9a841ff84858f44
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-06 15:14:56 -0600

    display event participants, event page layout

--- apps/web/app/(dashboard)/admin/eventos/[eventId]/page.tsx
+++ apps/web/app/(dashboard)/admin/eventos/[eventId]/page.tsx
@@ -13,15 +13,27 @@ import IconSquare from '@/components/icon-square';
 import LoadingMessage from '@/components/loading-message';
 import { PageContent } from '@/components/page-content';
 import ProductCard from '@/components/product-card';
-import { ProductsBoard } from '@/components/products-board';
+
+import { ProfileInfo } from '@/components/profile-info';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import {
   Dialog,
   DialogContent,
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import {
   Empty,
   EmptyDescription,
@@ -31,8 +43,19 @@ import {
 } from '@/components/ui/empty';
 import { Separator } from '@/components/ui/separator';
 import { useDeleteEventActivity, useGetEventById } from '@/hooks/events';
-import { IActivity, IProduct } from '@repo/types';
-import { File, FileText, ListTodo, Newspaper, Shapes } from 'lucide-react';
+import { IActivity, IProduct, IUser } from '@repo/types';
+import {
+  Download,
+  File,
+  FileText,
+  ListTodo,
+  MoreHorizontal,
+  Newspaper,
+  Shapes,
+  Upload,
+  UserMinus,
+  Users,
+} from 'lucide-react';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
@@ -68,93 +91,218 @@ const Page = () => {
           </Header>
 
           <PageContent className="px-4">
-            <Card>
-              <CardHeader>
-                <div className="flex justify-between">
-                  <div className="flex gap-3 items-center">
-                    <IconSquare className="bg-green-50 text-green-700">
-                      <ListTodo />
-                    </IconSquare>
+            <div className="flex gap-6 lg:gap-4 flex-col lg:flex-row">
+              <div className="w-full lg:max-w-md flex flex-col gap-6">
+                <Card>
+                  <CardHeader>
+                    <div className="flex justify-between">
+                      <div className="flex gap-3 items-center">
+                        <IconSquare className="bg-purple-50 text-purple-700">
+                          <FileText />
+                        </IconSquare>
 
-                    <CardTitle>Actividades</CardTitle>
-                  </div>
-                  <Dialog>
-                    <DialogTrigger className="h-7 px-3 bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground border-transparent">
-                      Crear
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogTitle>Nueva Actividad</DialogTitle>
-                      <Separator />
-                      <ActivityForm eventId={eventId} />
-                    </DialogContent>
-                  </Dialog>
-                </div>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-col gap-4">
-                  {event.activities?.length ? (
-                    event.activities.map((a: IActivity) => (
-                      <ActivityCard
-                        key={a._id}
-                        activity={a}
-                        onDelete={handleDeleteActivity}
-                      />
-                    ))
-                  ) : (
-                    <Empty>
-                      <EmptyHeader>
-                        <EmptyMedia variant="icon">
+                        <CardTitle>Informe</CardTitle>
+                      </div>
+                      <Dialog>
+                        <DialogTrigger className="h-7 px-3 bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground border-transparent">
+                          <Upload />
+                          Subir
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogTitle>Nueva Actividad</DialogTitle>
+                          <Separator />
+                          <ActivityForm eventId={eventId} />
+                        </DialogContent>
+                      </Dialog>
+                    </div>
+                  </CardHeader>
+
+                  <CardContent>
+                    {event.report ? (
+                      <span>reporte</span>
+                    ) : (
+                      <Empty>
+                        <EmptyHeader>
+                          <EmptyMedia variant="icon">
+                            <FileText />
+                          </EmptyMedia>
+                          <EmptyTitle>No Hay Informe</EmptyTitle>
+                          <EmptyDescription>
+                            No se ha subido el informe del evento. Haz clic en
+                            subir para seleccionar un archivo.
+                          </EmptyDescription>
+                        </EmptyHeader>
+                      </Empty>
+                    )}
+                  </CardContent>
+                </Card>
+
+                <Card>
+                  <CardHeader>
+                    <div className="flex justify-between">
+                      <div className="flex gap-3 items-center">
+                        <IconSquare className="bg-blue-50 text-blue-700">
+                          <Users />
+                        </IconSquare>
+
+                        <CardTitle>Participantes</CardTitle>
+                      </div>
+                      {/* <Dialog>
+                        <DialogTrigger className="h-7 px-3 bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground border-transparent">
+                          Agregar
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogTitle>Participación</DialogTitle>
+                          <Separator />
+                        </DialogContent>
+                      </Dialog> */}
+                    </div>
+                  </CardHeader>
+
+                  <CardContent>
+                    {event.participants.length > 0 ? (
+                      <div className="flex flex-col gap-3">
+                        {event.participants.map((p: IUser) => (
+                          <div key={p._id} className="flex justify-between">
+                            <ProfileInfo
+                              givenName={p.givenName}
+                              familyName={p.familyName}
+                              avatarUrl={p.avatarUrl}
+                              email={p.email}
+                            />
+
+                            <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                                <Button size="icon-sm" variant="ghost">
+                                  <MoreHorizontal />
+                                </Button>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent>
+                                <Button
+                                  size="sm"
+                                  className="w-full justify-start font-normal bg-transparent hover:text-destructive-foreground"
+                                  variant="ghost"
+                                  disabled={false}
+                                  onClick={() =>
+                                    // mutation
+                                    console.log('Expularr')
+                                  }
+                                >
+                                  <UserMinus /> Expulsar
+                                </Button>
+                              </DropdownMenuContent>
+                            </DropdownMenu>
+                          </div>
+                        ))}
+                      </div>
+                    ) : (
+                      <Empty>
+                        <EmptyHeader>
+                          <EmptyMedia variant="icon">
+                            <Users />
+                          </EmptyMedia>
+                          <EmptyTitle>No Hay Participantes</EmptyTitle>
+                          <EmptyDescription>
+                            No se han agregado participantes al evento. Agrega
+                            participantes.
+                          </EmptyDescription>
+                        </EmptyHeader>
+                      </Empty>
+                    )}
+                  </CardContent>
+                </Card>
+              </div>
+
+              <div className="w-full flex flex-col gap-6">
+                <Card>
+                  <CardHeader>
+                    <div className="flex justify-between">
+                      <div className="flex gap-3 items-center">
+                        <IconSquare className="bg-green-50 text-green-700">
                           <ListTodo />
-                        </EmptyMedia>
-                        <EmptyTitle>No Hay Actividades</EmptyTitle>
-                        <EmptyDescription>
-                          No se han agregado actividades para el evento. Crea
-                          una nueva actividad.
-                        </EmptyDescription>
-                      </EmptyHeader>
-                    </Empty>
-                  )}
-                </div>
-              </CardContent>
-            </Card>
+                        </IconSquare>
 
-            <Card>
-              <CardHeader>
-                <div className="flex gap-3 items-center">
-                  <IconSquare className="bg-orange-50 text-orange-700">
-                    <Shapes />
-                  </IconSquare>
+                        <CardTitle>Actividades</CardTitle>
+                      </div>
+                      <Dialog>
+                        <DialogTrigger className="h-7 px-3 bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground border-transparent">
+                          Crear
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogTitle>Nueva Actividad</DialogTitle>
+                          <Separator />
+                          <ActivityForm eventId={eventId} />
+                        </DialogContent>
+                      </Dialog>
+                    </div>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="flex flex-col gap-4">
+                      {event.activities?.length ? (
+                        event.activities.map((a: IActivity) => (
+                          <ActivityCard
+                            key={a._id}
+                            activity={a}
+                            onDelete={handleDeleteActivity}
+                          />
+                        ))
+                      ) : (
+                        <Empty>
+                          <EmptyHeader>
+                            <EmptyMedia variant="icon">
+                              <ListTodo />
+                            </EmptyMedia>
+                            <EmptyTitle>No Hay Actividades</EmptyTitle>
+                            <EmptyDescription>
+                              No se han agregado actividades para el evento.
+                              Crea una nueva actividad.
+                            </EmptyDescription>
+                          </EmptyHeader>
+                        </Empty>
+                      )}
+                    </div>
+                  </CardContent>
+                </Card>
 
-                  <CardTitle>Productos</CardTitle>
-                </div>
-              </CardHeader>
-              <CardContent>
-                <div className="px-4">
-                  {event.products.length > 0 ? (
-                    event.products.map((p: IProduct) => (
-                      <ProductCard
-                        key={p._id}
-                        product={p}
-                        projectId={p.projectId}
-                      />
-                    ))
-                  ) : (
-                    <Empty>
-                      <EmptyHeader>
-                        <EmptyMedia variant="icon">
-                          <Shapes />
-                        </EmptyMedia>
-                        <EmptyTitle>No Hay Productos</EmptyTitle>
-                        <EmptyDescription>
-                          No se ha agregado ningún producto. Debes esperar a que
-                          los asistentes seleccionen sus productos.
-                        </EmptyDescription>
-                      </EmptyHeader>
-                    </Empty>
-                  )}
-                </div>
-              </CardContent>
-            </Card>
+                <Card>
+                  <CardHeader>
+                    <div className="flex gap-3 items-center">
+                      <IconSquare className="bg-orange-50 text-orange-700">
+                        <Shapes />
+                      </IconSquare>
+
+                      <CardTitle>Productos</CardTitle>
+                    </div>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="px-4">
+                      {event.products.length > 0 ? (
+                        event.products.map((p: IProduct) => (
+                          <ProductCard
+                            key={p._id}
+                            product={p}
+                            projectId={p.projectId}
+                          />
+                        ))
+                      ) : (
+                        <Empty>
+                          <EmptyHeader>
+                            <EmptyMedia variant="icon">
+                              <Shapes />
+                            </EmptyMedia>
+                            <EmptyTitle>No Hay Productos</EmptyTitle>
+                            <EmptyDescription>
+                              No se ha agregado ningún producto. Debes esperar a
+                              que los asistentes seleccionen sus productos.
+                            </EmptyDescription>
+                          </EmptyHeader>
+                        </Empty>
+                      )}
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
           </PageContent>
         </>
       )}
