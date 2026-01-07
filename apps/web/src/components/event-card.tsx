314527776e1345c98c146536e132bc9076671beb
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-07 13:37:08 -0600

    add event card

---
+++ apps/web/src/components/event-card.tsx
@@ -0,0 +1,167 @@
+import { BadgeVariants, IEvent, IUser } from '@repo/types';
+import { userProfile } from 'context/profile-provider';
+import { format } from 'date-fns';
+import { es } from 'date-fns/locale';
+import {
+  ArrowUpRight,
+  Calendar,
+  CheckSquare,
+  Info,
+  MapPin,
+  Shapes,
+} from 'lucide-react';
+import Link from 'next/link';
+import AvatarRow from './avatar-row';
+import IconSquare from './icon-square';
+import { Badge } from './ui/badge';
+import { Button } from './ui/button';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import CopyButton from './ui/copy';
+
+interface EventCardProps {
+  event: IEvent;
+}
+
+export function EventCard({ event }: EventCardProps) {
+  /**
+   * Context
+   */
+  const { user } = userProfile();
+
+  /**
+   * Conditionally render buttons
+   */
+  const currentUserId = user._id;
+  const isOwner = event.createdBy._id === currentUserId;
+  const isParticipant = event.participants.some(
+    (p: IUser) => p._id === currentUserId,
+  );
+
+  const renderActionButton = () => {
+    if (isOwner || isParticipant) {
+      return (
+        <Button variant="ghost" size="sm" asChild>
+          <Link href={`/user/eventos/${event._id}`}>
+            <span className="flex gap-1 items-center">
+              Visitar
+              <ArrowUpRight />
+            </span>
+          </Link>
+        </Button>
+      );
+    } else {
+      return (
+        <Button size="sm" disabled={event.isPrivate}>
+          {event.isPrivate ? 'Evento Privado' : 'Inscribirse'}
+        </Button>
+      );
+    }
+  };
+
+  let badgeVariant:
+    | 'default'
+    | 'secondary'
+    | 'destructive'
+    | 'outline'
+    | 'green'
+    | 'gray'
+    | 'purple'
+    | 'orange'
+    | 'blue'
+    | null
+    | undefined;
+  switch (event.isPrivate) {
+    case true:
+      badgeVariant = BadgeVariants.PURPLE;
+      break;
+    case false:
+      badgeVariant = BadgeVariants.BLUE;
+      break;
+  }
+
+  return (
+    <Card className="w-full gap-4 min-w-96">
+      <CardHeader>
+        <div className="flex justify-between">
+          <div className="flex gap-2 items-start">
+            <IconSquare className="bg-blue-50 text-blue-700">
+              <Calendar />
+            </IconSquare>
+            <div className="flex flex-col gap-1">
+              <CardTitle className="line-clamp-1 leading-5 h-6">
+                {event.name}
+              </CardTitle>
+              <CardDescription className="text-xs line-clamp-1">
+                {event?.organization}
+              </CardDescription>
+            </div>
+          </div>
+          <Badge variant={badgeVariant} className="h-6">
+            {event.isPrivate ? 'Privado' : 'Público'}
+          </Badge>
+        </div>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4 h-full text-sm text-muted-foreground">
+        <div className="flex gap-2">
+          <Info className="shrink-0" size={14} />
+          <CardDescription className="h-26 line-clamp-5">
+            {event.summary}
+          </CardDescription>
+        </div>
+
+        <div className="flex gap-2 group items-center">
+          <MapPin className="shrink-0" size={14} />
+          <span>{event.location}</span>
+          <CopyButton
+            valueToCopy={event.location}
+            className="group-hover:opacity-100 opacity-0"
+          />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <Calendar size={14} />
+          {event.endDate ? (
+            <div>
+              {format(event.startDate, "d 'de' MMMM 'al' ", {
+                locale: es,
+              })}
+              {format(event.endDate, "d 'de' MMMM 'de' yyyy", {
+                locale: es,
+              })}
+            </div>
+          ) : (
+            <div>
+              {format(event.startDate, "d 'de' MMMM 'de' yyyy", {
+                locale: es,
+              })}
+            </div>
+          )}
+        </div>
+      </CardContent>
+      <CardFooter className="flex border-t gap-3 justify-between items-center">
+        <div className="flex gap-3">
+          <AvatarRow profiles={event.participants} />
+
+          <span className="flex gap-1 items-center justify-center text-xs text-muted-foreground">
+            <CheckSquare size={14} />
+            {event.activities?.length}
+          </span>
+
+          <span className="flex gap-1 items-center justify-center text-xs text-muted-foreground">
+            <Shapes size={14} />
+            {event.products?.length}
+          </span>
+        </div>
+        <CardAction>{renderActionButton()}</CardAction>
+      </CardFooter>
+    </Card>
+  );
+}
