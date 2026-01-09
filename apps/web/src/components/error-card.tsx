d2cd72db0d45926e6f01d0023ac0b167d714cd49
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-09 13:55:58 -0600

    add new error card

---
+++ apps/web/src/components/error-card.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { MessageCircleX, OctagonX, RotateCw } from 'lucide-react';
+import { Dialog, DialogTitle, DialogTrigger, DialogContent } from './ui/dialog';
+
+export default function ErrorCard() {
+  return (
+    <Card className="text-red-700 border-red-200 max-w-sm">
+      <CardHeader>
+        <div className="flex gap-2 items-center">
+          <div className="flex items-center justify-center w-fit bg-red-100 text-red-700 rounded-full p-2">
+            <OctagonX size={16} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <CardTitle>Ocurrió un Error</CardTitle>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="text-sm flex flex-col">
+        <div className="flex gap-2 items-center">
+          <RotateCw size={14} className="shrink-0" />
+          <span>Intenta refrescar la página</span>
+        </div>
+        <div className="flex gap-2 items-center">
+          <MessageCircleX size={14} className="shrink-0" />
+          <span>
+            Si el error persiste puedes
+            <Dialog>
+              <DialogTrigger className="px-1 hover:bg-transparent hover:text-destructive-foreground hover:underline">
+                levantar un reporte
+              </DialogTrigger>
+
+              <DialogContent>
+                <DialogTitle>Reporte</DialogTitle>
+              </DialogContent>
+            </Dialog>
+          </span>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
