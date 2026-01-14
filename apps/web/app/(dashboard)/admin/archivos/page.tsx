19351fc40ccc5364161fe650f34b976cfa071d5b
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-14 09:48:31 -0600

    make files page ssr

--- apps/web/app/(dashboard)/admin/archivos/page.tsx
+++ apps/web/app/(dashboard)/admin/archivos/page.tsx
@@ -1,17 +1,12 @@
-'use client';
-
 import FilesTable from '@/components/files-table';
 import {
   Header,
-  HeaderAction,
-  HeaderContent,
   HeaderDescription,
   HeaderHeading,
   HeaderTitle,
 } from '@/components/header';
 import { PageContent } from '@/components/page-content';
-import TeamsTable from '@/components/teams-table';
-import { Button } from '@/components/ui/button';
+
 import React from 'react';
 
 const Page = () => {
