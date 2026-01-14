32359904b3c0f39a2a54a34c24c2a6b031e607fa
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-14 10:29:11 -0600

    add file compound components to display files

--- apps/web/app/(dashboard)/admin/proyectos/[projectId]/page.tsx
+++ apps/web/app/(dashboard)/admin/proyectos/[projectId]/page.tsx
@@ -3,6 +3,7 @@
 import { ActivitiesBoard } from '@/components/activities-board';
 import { CardMembers } from '@/components/card-members';
 import ErrorCard from '@/components/error-card';
+import FileList from '@/components/file-list';
 import { Header, HeaderAction, HeaderHeading } from '@/components/header';
 import LoadingMessage from '@/components/loading-message';
 import { PageContent } from '@/components/page-content';
@@ -17,8 +18,10 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
+import { useGetFiles } from '@/hooks/files';
 import { useProject } from '@/hooks/projects';
 import { calculateProgress } from '@/lib/utils';
+import { IFile } from '@repo/types';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
@@ -29,6 +32,11 @@ const Page = () => {
     isLoading: loadingProject,
     isError,
   } = useProject(projectId);
+  //   const {
+  //     data: files,
+  //     isLoading: loadingFiles,
+  //     isError: errorFetchingFiles,
+  //   } = useGetFiles();
 
   return (
     <div className="w-full h-full">
