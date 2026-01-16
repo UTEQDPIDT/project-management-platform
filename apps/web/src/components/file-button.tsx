b6a0d8a6fd6b3f8e141d1f9bbc5cf6c20553de49
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-15 20:37:30 -0600

    add button to display and download product file

---
+++ apps/web/src/components/file-button.tsx
@@ -0,0 +1,50 @@
+import { IFile } from '@repo/types';
+import React, { useState } from 'react';
+import { Button } from './ui/button';
+import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
+import { downloadFile } from '@/services/files.service';
+import { Paperclip } from 'lucide-react';
+import LoadingMessage from './loading-message';
+
+type FileButtonProps = {
+  file: IFile;
+};
+
+export default function FileButton({ file }: FileButtonProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleDownload = async () => {
+    try {
+      setIsLoading(true);
+      await downloadFile(file._id, file.originalName);
+    } catch (error) {
+      console.error('Failed to download file:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button
+          size="sm"
+          variant="secondary"
+          onClick={handleDownload}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <LoadingMessage message="descargando" />
+          ) : (
+            <div className="flex items-center gap-1">
+              <Paperclip />
+              <span className="line-clamp-1">{file.originalName}</span>
+            </div>
+          )}
+        </Button>
+      </TooltipTrigger>
+
+      <TooltipContent>Descargar</TooltipContent>
+    </Tooltip>
+  );
+}
