f0574c11e762227be35fe060f8aa26117bab2f0c
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-20 09:19:55 -0600

    create component that displays team requests and handles accept or reject

---
+++ apps/web/src/components/team-notifications.tsx
@@ -0,0 +1,50 @@
+import {
+  ITeam,
+  ITeamMembership,
+  TeamMembershipRole,
+  TeamMembershipStatus,
+} from '@repo/types';
+import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Button } from './ui/button';
+import { Bell } from 'lucide-react';
+import { TeamUserRequests } from './team-user-requests';
+
+type TeamNotificationsProps = {
+  team: ITeam;
+};
+
+export function TeamNotifications({ team }: TeamNotificationsProps) {
+  const requests = team.memberships.filter(
+    (m: ITeamMembership) =>
+      m.role === TeamMembershipRole.MEMBER &&
+      m.status === TeamMembershipStatus.PENDING,
+  );
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon-sm">
+          <Bell />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Notificaciones</DropdownMenuLabel>
+        {requests.length > 0 ? (
+          <TeamUserRequests teamId={team._id} requests={requests} />
+        ) : (
+          <div className="px-2 py-3">
+            <span className="text-muted-foreground text-sm">
+              No hay notificaciones
+            </span>
+          </div>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
