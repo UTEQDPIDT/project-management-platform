83a8a08eb0ab428d223147164b8653c1f7b0445b
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-21 14:42:17 -0600

    update frontend file services to handle new purpose property

--- packages/types/src/upload-file-payload.type.ts
+++ packages/types/src/upload-file-payload.type.ts
@@ -1,7 +1,9 @@
 import { EntityType } from './enums/entity-type.enum';
+import { FilePurpose } from './enums/file-purpose.enum';
 
 export type UploadFilePayload = {
   file: File;
   entityId: string;
   entityType: EntityType;
+  purpose?: FilePurpose;
 };
