b412ab7b59186d4d8e88052f9e0ac110722e4f6d
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-22 15:26:06 -0600

    add prep logo

--- apps/web/src/components/nav-logo.tsx
+++ apps/web/src/components/nav-logo.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 export function NavLogo() {
   return (
-    <div className="w-full flex items-center justify-start gap-3 select-none">
+    <div className="w-full flex items-center justify-start gap-3 select-none h-10">
       <Image
-        width={40}
-        height={40}
+        width={100}
+        height={50}
         className="shrink-0"
-        src="/uteq-logo.svg"
+        src="/prep-logo-negro.svg"
         alt="Logo de la UTEQ"
       />
       {/* <span className="text-base font-semibold leading-tight truncate">
