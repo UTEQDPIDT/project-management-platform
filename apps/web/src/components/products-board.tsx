b9d239e78b05fe69b70870130481016d873992b5
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-22 16:26:17 -0600

    ui enhancements to product related cards

--- apps/web/src/components/products-board.tsx
+++ apps/web/src/components/products-board.tsx
@@ -59,7 +59,7 @@ export function ProductsBoard({
         ) : isError ? (
           <ErrorCard />
         ) : products.length > 0 ? (
-          <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {products.map((p: IProduct) => (
               <ProductCard
                 key={p._id}
