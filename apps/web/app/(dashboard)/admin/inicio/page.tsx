e89b6d91943fd7881401b88df59147b9950482d3
Aeon Clouzeau <aeonruizc@gmail.com>
2026-01-26 10:45:50 -0600

    add boards to admin dashboard

--- apps/web/app/(dashboard)/admin/inicio/page.tsx
+++ apps/web/app/(dashboard)/admin/inicio/page.tsx
@@ -1,27 +1,66 @@
 'use client';
 
+import EventsBoard from '@/components/events-board';
 import {
   Header,
   HeaderHeading,
   HeaderTitle,
   HeaderDescription,
 } from '@/components/header';
+import { PageContent } from '@/components/page-content';
+import { ProjectsBoard } from '@/components/projects-board';
+import TeamsBoard from '@/components/teams-board';
+import { useEventsByUser } from '@/hooks/events';
+import { useProjectsByOwner } from '@/hooks/projects';
+import { useTeamsByUser } from '@/hooks/team';
 import { userProfile } from 'context/profile-provider';
 import React from 'react';
 
 const Page = () => {
   const { user } = userProfile();
+  const {
+    data: projects,
+    isLoading: loadingProjects,
+    isError: errorFetchingProjects,
+  } = useProjectsByOwner();
+  const {
+    data: teams,
+    isLoading: loadingTeams,
+    isError: errorFetchingTeams,
+  } = useTeamsByUser();
+  const {
+    data: events,
+    isLoading: loadingEvents,
+    isError: errorFetchingEvents,
+  } = useEventsByUser();
 
   return (
     <div>
       <Header>
         <HeaderHeading>
-          <HeaderTitle>Dashboard Administrativo</HeaderTitle>
+          <HeaderTitle>Panel de Control Administrativo</HeaderTitle>
           <HeaderDescription>
             Bienvenido {user.givenName}. Esto es lo que esta sucediendo.
           </HeaderDescription>
         </HeaderHeading>
       </Header>
+      <PageContent>
+        <ProjectsBoard
+          projects={projects}
+          loading={loadingProjects}
+          error={errorFetchingProjects}
+        />
+        <TeamsBoard
+          teams={teams}
+          isLoading={loadingTeams}
+          isError={errorFetchingTeams}
+        />
+        <EventsBoard
+          events={events}
+          isLoading={loadingEvents}
+          isError={errorFetchingEvents}
+        />
+      </PageContent>
     </div>
   );
 };
