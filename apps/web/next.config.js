4738a11d6a040dc0cafb4d82445132ecb4371361
Aeon Clouzeau <aeonruizc@gmail.com>
2026-02-04 16:37:56 -0600

    enable standalone output option for docker builds of nextjs app

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['http://localhost:3000'],
-  // Enable standalone output for Docker
-  //   output: 'standalone', <- only for production builds
-  transpilePackages: ['@repo/ui'],
+  // Enable standalone output for Docker (only for production builds)
+  output: 'standalone',
+  //   transpilePackages: ['@repo/ui'],
 };
 
 export default nextConfig;
