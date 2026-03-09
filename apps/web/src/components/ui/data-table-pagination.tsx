78369003f18de5d8e8256ed53d770d6e8585ed8c
Aeon Clouzeau <aeonruizc@gmail.com>
2026-03-09 13:14:48 -0600

    change export

--- apps/web/src/components/ui/data-table-pagination.tsx
+++ apps/web/src/components/ui/data-table-pagination.tsx
@@ -18,7 +18,7 @@ interface DataTablePaginationProps<TData> {
   table: Table<TData>;
 }
 
-export default function DataTablePagination<TData>({
+export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
