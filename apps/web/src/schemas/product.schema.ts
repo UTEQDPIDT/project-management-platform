bf1d77140623897b8ba0e83b5154636d44a42477
AmirLoopez <144493609+AmirLoopez@users.noreply.github.com>
2026-03-18 12:26:03 -0600

    update: file size for product file in product zod schema

--- apps/web/src/schemas/product.schema.ts
+++ apps/web/src/schemas/product.schema.ts
@@ -12,8 +12,8 @@ export const getProductSchema = (isUpdate: boolean) =>
       ? z
           .instanceof(File)
           .refine(
-            (file) => file.size <= 2 * 1024 * 1024,
-            'El archivo pesa más de 2 MB',
+            (file) => file.size <= 5 * 1024 * 1024,
+            'El archivo pesa más de 5 MB',
           )
           .refine(
             (file) => file.type === 'application/pdf',
@@ -23,8 +23,8 @@ export const getProductSchema = (isUpdate: boolean) =>
       : z
           .instanceof(File, { message: 'El archivo es obligatorio.' })
           .refine(
-            (file) => file.size <= 2 * 1024 * 1024,
-            'El archivo pesa más de 2 MB',
+            (file) => file.size <= 5 * 1024 * 1024,
+            'El archivo pesa más de 5 MB',
           )
           .refine(
             (file) => file.type === 'application/pdf',
