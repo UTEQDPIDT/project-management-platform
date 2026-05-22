5a6dfc972948bb550d74f89c9135d319b084d60f
AmirLoopez <144493609+AmirLoopez@users.noreply.github.com>
2026-05-22 09:36:04 -0600

    feat: add new optios to teams grade enum

--- packages/types/src/enums/teams-grade.enum.ts
+++ packages/types/src/enums/teams-grade.enum.ts
@@ -1,4 +1,7 @@
 export enum TeamsGrade {
   FORMACION = 'En Formación',
   CONSOLIDADO = 'Consolidado',
+  CA_EN_FORMACION = 'CA en Formación',
+  CA_CONSOLIDADO = 'CA Consolidado',
+  CA_EN_CONSOLIDACION = 'CA en Consolidación',
 }
