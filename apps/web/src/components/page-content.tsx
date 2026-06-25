ea1db7d0a8a731ac2a74a0d86db5f4572154d0a4
AmirLoopez <144493609+AmirLoopez@users.noreply.github.com>
2026-06-25 11:46:58 -0600

    feat(styles): change tailwind label

--- apps/web/src/components/page-content.tsx
+++ apps/web/src/components/page-content.tsx
@@ -12,7 +12,7 @@ export function PageContent({
     <div className="flex w-full justify-center">
       <div
         className={cn(
-          'flex flex-col gap-6 items-center w-full max-w-[1440px] px-4 py-6',
+          'flex flex-col gap-6 items-center w-full max-w-360 px-4 py-6',
           className,
         )}
       >
