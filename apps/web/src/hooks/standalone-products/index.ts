6572cee80a56497589a462f48db5def471876b8d
AmirLoopez <144493609+AmirLoopez@users.noreply.github.com>
2026-07-09 12:26:56 -0600

    feat(web): export standalone products hooks

---
+++ apps/web/src/hooks/standalone-products/index.ts
@@ -0,0 +1,6 @@
+export * from './use-standalone-products';
+export * from './use-standalone-product-by-id';
+export * from './use-standalone-products-by-user';
+export * from './use-create-standalone-product';
+export * from './use-update-standalone-product';
+export * from './use-delete-standalone-product';
