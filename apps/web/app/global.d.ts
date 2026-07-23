533ba8290edfff2b30ae5b9c98ab2991d8649333
AmirLoopez <144493609+AmirLoopez@users.noreply.github.com>
2026-07-23 15:23:09 -0600

    feat(web): add grecaptcha window typings

--- apps/web/app/global.d.ts
+++ apps/web/app/global.d.ts
@@ -1,1 +1,23 @@
 declare module '*.css';
+
+declare global {
+	interface Window {
+		grecaptcha?: {
+			ready: (callback: () => void) => void;
+			render: (
+				container: HTMLElement,
+				parameters: {
+					sitekey: string;
+					size: 'invisible';
+					callback: (token: string) => void;
+					'error-callback'?: () => void;
+					'expired-callback'?: () => void;
+				},
+			) => number;
+			execute: (widgetId: number) => void;
+			reset: (widgetId: number) => void;
+		};
+	}
+}
+
+export {};
